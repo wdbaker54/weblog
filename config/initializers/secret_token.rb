645591aefab6854ae610f6d16502398ce1195bfb
Bill Baker <Bill Baker bill@bydesignpublishing.com>
2010-09-15 16:55:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = 'bfa2d62532d8ecc3c9ce89f03f039a90ec10fa695920eba9f9b0c7f55d0763e15a72f8f5923ccadc571d88b53b788be40d3d1fbc2b9c1fce8c5b2d1f6443fa38'
